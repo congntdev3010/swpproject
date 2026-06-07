58cfe2d905287be2adb2c52457e440efa37b1bfb
congbth <congbth6868@gmail.com>
2026-06-07 22:35:04 +0700

    up
    connect db

--- Source/src/java/dao/DBContext.java
+++ Source/src/java/dao/DBContext.java
@@ -1,5 +1,4 @@
-package com.swp391.dao;
-
+package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,7 +6,7 @@ import java.sql.SQLException;
 public class DBContext {
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3307/pass_swp"
+    private static final String URL = "jdbc:mysql://localhost:3306/pass_swp"
             + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
     private static final String PASS = ""; // <-- điền password MySQL của bạn
